0e1503d15ee17ecdd77949605e7c6e72852499ed
MidhaTahir <midhatahirkhan2011@gmail.com>
2021-09-15 21:13:48 +0500

    add: brad crash course on strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8c435b6f0d1571e419317ca9a91b4695'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c979ef3f-1a4b-4a85-afb5-8fd76009cb8b'
+};
